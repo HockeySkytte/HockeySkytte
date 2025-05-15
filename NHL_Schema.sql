f25314fcd28874d6da9ada60bf39ed75926b3dac
HockeySkytte <lars.sunesen.skytte@gmail.com>
2025-05-15 21:43:40 +0200

    Add files via upload

---
+++ NHL_Schema.sql
@@ -0,0 +1,5 @@
+# Create a new Schema/Database for our tables. I've called the database Public, because I plan to put public data from NHL, PWHL and AHL here.
+CREATE SCHEMA `public`;
+
+# Select the newly created Schema.
+USE public;
